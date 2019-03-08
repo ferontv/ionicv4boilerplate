de69ad58c455a55dfe096ee04c09bba16c564521
Fernando Ontiveros <fernando@MacBook-Air-de-Fernando.local>
2019-03-07 23:01:41 -0600

    Mostrar Elemento Seleccionado en la Vista

--- src/app/pages/description-planets/description-planets.page.ts
+++ src/app/pages/description-planets/description-planets.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapiService } from '../../services/swapi.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-description-planets',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./description-planets.page.scss'],
 })
 export class DescriptionPlanetsPage implements OnInit {
-
-  constructor() { }
+  planets: any;
+  constructor(private route: ActivatedRoute, private swapiService: SwapiService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      let id = params.get('id');
+      console.log(id);
+
+      this.planets = this.swapiService.getSwapi(`planets/${id}`).subscribe(data => {
+        this.planets = data;
+        console.log(data)
+      });
+
+    })
   }
 
 }
